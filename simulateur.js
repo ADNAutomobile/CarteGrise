bafee0c90aa720cfaaa61c4c5a8844ed3826cd0b
ADNAutomobile <philippe@adnautomobile.com>
2025-04-03 10:41:09 +0200

    Update simulateur.js

--- simulateur.js
+++ simulateur.js
@@ -127,8 +127,8 @@ function calculer() {
   document.getElementById("resultat").innerHTML = `
     <div class="card card-body">
       <p><strong>Année barème :</strong> ${annee}</p>
-      <p><strong>Ancienneté :</strong> ${mois} mois -> ${decote}% abattement CO₂</p>
-      <p><strong>Malus CO₂ :</strong> ${malusFinal.toFixed(2)} €</p>
+      <p><strong>Vétusté :</strong> ${mois} mois -> ${decote}% abattement CO₂</p>
+      <p><strong>Malus :</strong> ${malusFinal.toFixed(2)} €</p>
       <p><strong>Taxe au poids :</strong> ${poidsTaxe.toFixed(2)} €</p>
       <p><strong>Taxe :</strong> ${taxe.toFixed(2)} €</p>
       <p><strong>Acheminement :</strong> ${acheminement.toFixed(2)} €</p>
